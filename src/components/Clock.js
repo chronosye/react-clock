1f29c09f4f98f78c48021d777b82ef360cc8286a
Uldis Jansons <jansons08@gmail.com>
2022-03-31 12:28:53 +0300

    added date

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,25 +2,39 @@ import { useState, useEffect } from "react";
 import classes from "./Clock.module.scss";
 
 const Clock = () => {
-  const getDate = () => {
+  const getTime = () => {
     return new Date().toLocaleTimeString("lv-LV", { hour12: false });
   };
 
-  const [time, setTime] = useState(getDate());
+  const getDate = () => {
+    const date = new Date().toLocaleString("default", {
+      day: "numeric",
+      month: "long",
+    });
+
+    return date;
+  };
+
+  const [time, setTime] = useState(getTime());
 
   useEffect(() => {
     setInterval(() => {
-      setTime(getDate());
+      setTime(getTime());
     }, 1000);
   }, []);
 
   return (
-    <div>
-      <div className={classes.clock}>
-        <p>{time}</p>
+    <div className={classes.all}>
+      <div className={classes.date}>
+        <p>{getDate()}</p>
       </div>
-      <div className={classes.clockBackground}>
-        <p>88:88:88</p>
+      <div className={classes.clockDiv}>
+        <div className={classes.clock}>
+          <p>{time}</p>
+        </div>
+        <div className={classes.clockBackground}>
+          <p>88:88:88</p>
+        </div>
       </div>
     </div>
   );
